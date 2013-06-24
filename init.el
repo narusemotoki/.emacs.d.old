36f4b7b8826c395d479b3c297e9bd592f38afd7f
Naruse Motoki <motoki@naru.se>
2013-06-24 23:24:12 +0900

    elscreenを追加

--- init.el
+++ init.el
@@ -7,6 +7,21 @@
 (global-set-key "\C-h" 'delete-backward-char)
 
 ;------------------------------------------------------------------------------
+; elisp以下にパスを通す
+; http://d.hatena.ne.jp/sandai/20120304/p2
+;------------------------------------------------------------------------------
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+; 複数指定する場合 => (add-to-load-path "elisp" "xxx" "xxx")
+(add-to-load-path "elisp")
+
+;------------------------------------------------------------------------------
 ; elpaのリポジトリ追加
 ;------------------------------------------------------------------------------
 (require 'package)
@@ -48,3 +63,6 @@
 (setq make-backup-files nil)
 ; .#*
 (setq auto-save-default nil)
+
+(require 'elscreen)
+(elscreen-start)
