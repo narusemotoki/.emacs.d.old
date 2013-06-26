d2e40d8d37d1a105e1fff7e68fc870419673cdbf
Naruse Motoki <motoki@naru.se>
2013-06-26 13:02:03 +0900

    基本的な設定を追加

--- inits/00_base.el
+++ inits/00_base.el
@@ -4,10 +4,72 @@
 (prefer-coding-system 'utf-8)
 (set-language-environment "Japanese")
 
+; 画像ファイルを表示
+(auto-image-file-mode t)
+
+; メニューバーを非表示にする
+(menu-bar-mode -1)
+
+; ツールバーを非表示にする
+(tool-bar-mode -1)
+
+; スクロールバーを非表示にする
+(toggle-scroll-bar nil)
+
+; 対応する括弧を光らせる。
+(show-paren-mode 1)
+
+; ウィンドウ内に収まらないときだけ括弧内も光らせる。
+(setq show-paren-style 'mixed)
+
+; 現在行を目立たせる
+(global-hl-line-mode)
+
+; カーソルの位置が何文字目かを表示する
+(column-number-mode t)
+
+; カーソルの位置が何行目かを表示する
+(line-number-mode t)
+
+; カーソルの場所を保存する
+(require 'saveplace)
+(setq-default save-place t)
+
+; 行の先頭でC-kを一回押すだけで行全体を消去する
+(setq kill-whole-line t)
+
+; インデントにスペースを使う
+(setq-default indent-tabs-mode nil)
+
+; 前の行と同じインデント
+(setq indent-line-function 'indent-relative-maybe)
+
+; 行番号の表示
+(global-linum-mode t)
+
+; yes or noをy or nにする
+(fset 'yes-or-no-p 'y-or-n-p)
+
+; 行末の空白を表示
+(setq-default show-trailing-whitespace t)
+
+; シンボリックリンクはその先のファイルを開く
+(setq vc-follow-symlinks t)
+
+; バッファにあるファイルが変更された場合，自動でバッファに読み直す
+(global-auto-revert-mode 1)
+
+; Shift + 矢印でウィンドウを移動する
+(setq windmove-wrap-around t)
+(windmove-default-keybindings)
+
 ;------------------------------------------------------------------------------
 ; キーバインドの変更
 ;------------------------------------------------------------------------------
 (global-set-key "\C-h" 'delete-backward-char)
+
+; Emacs外のiBusでC-\を使いたいので切る
+(global-set-key (kbd "C-\\") nil)
 
 ;------------------------------------------------------------------------------
 ; バックアップファイルを作らない
