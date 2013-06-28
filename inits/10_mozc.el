be7d511b9b65a364d6e9876d9882603af4db8381
Naruse Motoki <motoki@naru.se>
2013-06-28 22:57:11 +0900

    再びmozcの候補をエコーエリアに表示するようにした

--- inits/10_mozc.el
+++ inits/10_mozc.el
@@ -5,7 +5,7 @@
 ; 入力中のすぐ下に表示はelscreenと相性が悪いかもしれない
 ; elscreenとの起動順で解決するかも
 ; 候補が横に並んでしまい見にくい場合、これを有効にして候補をエコーエリアに表示
-; (setq mozc-candidate-styleh 'echo-area)
+(setq mozc-candidate-style 'echo-area)
 
 ; C-oでmozcの有効、無効を切り替える
 (global-set-key (kbd "C-o") 'toggle-input-method)
