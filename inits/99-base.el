e113cf18b250eb29f1a526b0169ab85ff23b1d8b
Naruse Motoki <motoki@naru.se>
2013-06-29 00:01:44 +0900

    mozcが無効になるのを修正

--- inits/99-base.el
+++ inits/99-base.el
@@ -1,7 +1,6 @@
 ;------------------------------------------------------------------------------
 ; 文字コードの設定が拡張によって上書きされるので、最後にもう一度UTF-8指定
 ;------------------------------------------------------------------------------
-(set-language-environment 'Japanese)
 (prefer-coding-system 'utf-8)
 (setq default-coding-systems 'utf-8)
 (setq file-name-coding-system 'utf-8)
