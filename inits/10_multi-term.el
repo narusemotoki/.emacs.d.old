37f2449c5ee78c259f581082074d1a54a96cc1a1
Naruse Motoki <motoki@naru.se>
2013-06-28 20:48:05 +0900

    multi-termでは行末のスペースを強調しないようにした

--- inits/10_multi-term.el
+++ inits/10_multi-term.el
@@ -3,3 +3,12 @@
 ;------------------------------------------------------------------------------
 (require 'multi-term)
 (setq multi-term-program "/usr/bin/zsh")
+
+; multi-termでは行末のスペースを強調しない
+(add-hook
+  'term-mode-hook
+  '(lambda()
+    (setq show-trailing-whitespace nil)
+  )
+)
+
