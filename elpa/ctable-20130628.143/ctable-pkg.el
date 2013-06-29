6418865cb5e729e76804db7d95a23f074fa52fde
Naruse Motoki <motoki@naru.se>
2013-06-29 12:18:22 +0900

    pythonの補完を有効にした

---
+++ elpa/ctable-20130628.143/ctable-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ctable" "20130628.143" "Table component for Emacs Lisp [github]" (quote nil))
--- inits/40_python.el
+++ inits/40_python.el
@@ -1,3 +1,11 @@
 ; flymake
 (require 'flymake-python-pyflakes)
 (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+
+; http://qiita.com/yuu116atlab/items/2a62cb880ac863dcc8ef
+; 補完
+(require 'jedi)
+(add-hook 'python-mode-hook 'jedi:setup)
+; これを入れるとjediを手動<C-tab>で立ち上げないといけない。
+; (setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
