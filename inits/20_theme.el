f8c5b173d11fd1ba784f59804cded97e9cd99c79
Naruse Motoki <motoki@naru.se>
2013-06-30 13:40:11 +0900

    現在行とカーソルの色を変更

--- inits/20_theme.el
+++ inits/20_theme.el
@@ -9,3 +9,23 @@
 
 ; 透過
 (add-to-list 'default-frame-alist '(alpha . 90))
+
+; 現在行を目立たせる
+(global-hl-line-mode)
+
+; 現在行の色
+; http://www.happytrap.jp/blogs/2011/08/29/5877/
+(defface hlline-face
+  '((((class color)
+      (background dark))
+     (:background "#333333"))
+    (((class color)
+      (background light))
+     (:background "#CC0066"))
+   (t
+     ()))
+  "*Face used by hl-line.");
+(setq hl-line-face 'hlline-face)
+
+; カーソルの色を変更する
+(set-cursor-color "#CC0066")
