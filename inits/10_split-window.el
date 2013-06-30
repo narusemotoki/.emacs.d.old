ae958b45d05c3e8192978fe84a65eef5909b2074
Naruse Motoki <motoki@naru.se>
2013-06-30 22:04:13 +0900

    画面を三分割できるようにした

---
+++ inits/10_split-window.el
@@ -0,0 +1,24 @@
+; 画面を三分割する
+; http://d.hatena.ne.jp/yascentur/20110621/1308585547
+(defun split-window-vertically-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-vertically)
+    (progn
+      (split-window-vertically
+       (- (window-height) (/ (window-height) num_wins)))
+      (split-window-vertically-n (- num_wins 1)))))
+(defun split-window-horizontally-n (num_wins)
+  (interactive "p")
+  (if (= num_wins 2)
+      (split-window-horizontally)
+    (progn
+      (split-window-horizontally
+       (- (window-width) (/ (window-width) num_wins)))
+      (split-window-horizontally-n (- num_wins 1)))))
+(global-set-key "\C-x@" '(lambda ()
+                           (interactive)
+                           (split-window-vertically-n 3)))
+(global-set-key "\C-x#" '(lambda ()
+                           (interactive)
+                           (split-window-horizontally-n 3)))
