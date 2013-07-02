ec8368633bd9fd9129b3e3cfdb714eee8464f2c9
Naruse Motoki <motoki.naruse@topgate.co.jp>
2013-07-02 11:14:28 +0900

    redo+をundo-treeに変更

--- inits/10_undo-tree.el
+++ inits/10_undo-tree.el
@@ -1,7 +1,11 @@
+; http://qiita.com/takc923/items/c3d64b55fc4f3a3b0838
 ; http://www11.atwiki.jp/s-irie/pages/18.html
 ; やり直し
-(require 'redo+)
-(global-set-key (kbd "C-+") 'redo)
+(require 'undo-tree)
+(global-undo-tree-mode t)
+
+(global-set-key (kbd "C->") 'undo-tree-redo)
+(global-set-key (kbd "C-<") 'undo-tree-undo)
 
 ; 過去のundoがredoされないようにする
 (setq undo-no-redo t)
