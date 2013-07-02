ad51a3d7e578c617926d3bbde84f1d46109429e2
Naruse Motoki <motoki.naruse@topgate.co.jp>
2013-07-02 10:36:27 +0900

    perlの正規表現で検索、置換できるようにした

---
+++ elpa/foreign-regexp-20130618.1921/foreign-regexp-autoloads.el
@@ -0,0 +1,18 @@
+;;; foreign-regexp-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("foreign-regexp-pkg.el" "foreign-regexp.el")
+;;;;;;  (20946 11490 216361))
+
+;;;***
+
+(provide 'foreign-regexp-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; foreign-regexp-autoloads.el ends here
---
+++ inits/10_foreign-regexp.el
@@ -0,0 +1,6 @@
+(require 'foreign-regexp)
+
+; Perlの正規表現を使う
+(custom-set-variables
+ '(foreign-regexp/regexp-type 'perl)
+ '(reb-re-syntax 'foreign-regexp))
