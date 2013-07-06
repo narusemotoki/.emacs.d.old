8cc187f6509f1822e64e49e5e73081bf30abab7e
Naruse Motoki <motoki@naru.se>
2013-07-06 17:15:47 +0900

    twittering-modeで自動的に開くタイムラインと通知を設定

--- inits/10_twitter.el
+++ inits/10_twitter.el
@@ -22,10 +22,22 @@
   )
 )
 
+; 自動的に開くタイムライン
+; http://www.emacswiki.org/emacs/TwitteringMode-ja#toc26
+(setq twittering-initial-timeline-spec-string
+      '(":home"
+        ":replies"
+        ":favorites"
+;        ":direct_messages"
+;        ":search/emacs/"
+;        "user_name/list_name"
+))
 
-
-
-
-
-
-
+; 通知
+(add-hook 'twittering-new-tweets-hook (lambda ()
+  (let ((n twittering-new-tweets-count))
+    (start-process "twittering-notify" nil "notify-send"
+                   "-i" "/usr/share/pixmaps/gnome-emacs.png"
+                   "New tweets"
+                   (format "You have %d new tweet%s"
+                           n (if (> n 1) "s" ""))))))
