8b5d2b4884172de81abe5fe6cca76ea6f109541f
Naruse Motoki <motoki@naru.se>
2013-07-06 11:44:59 +0900

    説明をコメントにしていなくてエラーになっていたのを直した

--- inits/10_auto-highlighting-symbol.el
+++ inits/10_auto-highlighting-symbol.el
@@ -5,14 +5,14 @@
 ; ハイライトするまでの時間
 (ahs-set-idle-interval 0.5)
 
-| キー        | function                | 機能                                                                           |
-|-------------+-------------------------+--------------------------------------------------------------------------------|
-| M-<left>    | ahs-backward            | 前のシンボルへ移動                                                             |
-| M-<right>   | ahs-forward             | 次のシンボルへ移動                                                             |
-| M-s-<left>  | ahs-backward-difinition | ?                                                                              |
-| M-s-<right> | ahs-forward-definition  | ?                                                                              |
-| M--         | ahs-back-to-start       | 最初のカーソル位置のシンボルへ移動                                             |
-| C-x C-'     | ahs-change-range        | ハイライトする範囲を表示しているディスプレイの範囲かバッファ全体かを切り替える |
-| C-x C-a     | ahs-edit-mode           | ハイライトしているシンボルを一括でrenameする                                   |
+; | キー        | function                | 機能                                                                           |
+; |-------------+-------------------------+--------------------------------------------------------------------------------|
+; | M-<left>    | ahs-backward            | 前のシンボルへ移動                                                             |
+; | M-<right>   | ahs-forward             | 次のシンボルへ移動                                                             |
+; | M-s-<left>  | ahs-backward-difinition | ?                                                                              |
+; | M-s-<right> | ahs-forward-definition  | ?                                                                              |
+; | M--         | ahs-back-to-start       | 最初のカーソル位置のシンボルへ移動                                             |
+; | C-x C-'     | ahs-change-range        | ハイライトする範囲を表示しているディスプレイの範囲かバッファ全体かを切り替える |
+; | C-x C-a     | ahs-edit-mode           | ハイライトしているシンボルを一括でrenameする                                   |
 
 
