ef6dc7638615219f80ea3fafbfc059fb36b05e8b
Naruse Motoki <motoki@naru.se>
2013-07-15 08:58:14 +0900

    python-modeのインデントとバックスペースを設定

--- inits/40_python.el
+++ inits/40_python.el
@@ -11,3 +11,30 @@
 (setq jedi:complete-on-dot t)
 
 (add-hook 'python-mode-hook 'highlight-indentation-mode)
+
+(add-hook 'python-mode-hook
+    '(lambda ()
+        ; インデントを考えてくれる
+        (define-key (current-local-map) "\C-h" 'python-backspace)
+    ))
+
+
+; indentation
+(defadvice python-calculate-indentation (around outdent-closing-brackets)
+  "Handle lines beginning with a closing bracket and indent them so that
+  they line up with the line containing the corresponding opening bracket."
+(save-excursion
+  (beginning-of-line)
+  (let ((syntax (syntax-ppss)))
+    (if (and (not (eq 'string (syntax-ppss-context syntax)))
+             (python-continuation-line-p)
+             (cadr syntax)
+             (skip-syntax-forward "-")
+             (looking-at "\\s)"))
+        (progn
+          (forward-char 1)
+          (ignore-errors (backward-sexp))
+          (setq ad-return-value (current-indentation)))
+      ad-do-it))))
+
+(ad-activate 'python-calculate-indentation)
