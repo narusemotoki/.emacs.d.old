c7f605f1d7020f12d4cec431c96caba5f8d83eb7
Naruse Motoki <motoki.naruse@topgate.co.jp>
2013-08-06 15:25:29 +0900

    保存するディレクトリがない場合に作成していいか尋ねるようにした

--- inits/00_base.el
+++ inits/00_base.el
@@ -90,3 +90,13 @@
 (setq make-backup-files nil)
 ; .#*
 (setq auto-save-default nil)
+
+; 保存しようとしたファイルのディレクトリがなければ作成する
+; http://stackoverflow.com/questions/6830671/how-to-make-emacs-create-intermediate-dirs-when-saving-a-file
+(add-hook 'before-save-hook
+          (lambda ()
+            (when buffer-file-name
+              (let ((dir (file-name-directory buffer-file-name)))
+                (when (and (not (file-exists-p dir))
+                           (y-or-n-p (format "Directory %s does not exist. Create it?" dir)))
+                  (make-directory dir t))))))
