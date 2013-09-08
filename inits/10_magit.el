144a3912b22cf78d001c819548e594aca7b8fcba
Naruse Motoki <motoki@naru.se>
2013-09-08 12:55:22 +0900

    magit-statusをC-x gで起動するようにした

--- inits/10_magit.el
+++ inits/10_magit.el
@@ -1,3 +1,5 @@
 ; rebase-modeに自動で入らないようにする
 ;(setq auto-mode-alist (delete '("git-rebase-todo" . rebase-mode)
 ;                              auto-mode-alist))
+
+(global-set-key "\C-xg" 'magit-status)
