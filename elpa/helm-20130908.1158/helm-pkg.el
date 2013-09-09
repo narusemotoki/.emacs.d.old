42d84c1e64bc5b994aba99745d5d5a13cbf3412d
Naruse Motoki <motoki@naru.se>
2013-09-09 22:54:36 +0900

    パッケージのアップデート

---
+++ elpa/helm-20130908.1158/helm-pkg.el
@@ -0,0 +1,2 @@
+(define-package "helm" "20130908.1158" "Helm is an Emacs incremental and narrowing framework [github]" 'nil)
+
---
+++ elpa/w3m-20130908.2330/w3m-pkg.el
@@ -0,0 +1,2 @@
+(define-package "w3m" "20130908.2330" "an Emacs interface to w3m [cvs]" 'nil)
+
