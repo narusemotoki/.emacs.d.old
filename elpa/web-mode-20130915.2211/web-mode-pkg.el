f5e0296bb5f447b01fc605d94a5245188636fdf1
Naruse Motoki <motoki@naru.se>
2013-09-17 21:24:51 +0900

    パッケージのアップデート

--- elpa/web-mode-20130915.2211/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "web-mode" "20130915.2211" "major mode for editing html templates [github]" 'nil)
-
---
+++ elpa/web-mode-20130916.2256/web-mode-pkg.el
@@ -0,0 +1,2 @@
+(define-package "web-mode" "20130916.2256" "major mode for editing html templates [github]" 'nil)
+
