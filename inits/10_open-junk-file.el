71f5192c2d2c98db4ae2b591b67b395a644a4c52
Naruse Motoki <motoki@naru.se>
2013-10-23 13:45:00 +0900

    junkファイルに.txt拡張子をつけるようにした

--- inits/10_open-junk-file.el
+++ inits/10_open-junk-file.el
@@ -1,4 +1,4 @@
 (require 'open-junk-file)
-(setq open-junk-file-format "~/Documents/junk/%Y%m%d%H%M%S")
+(setq open-junk-file-format "~/Documents/junk/%Y%m%d%H%M%S.txt")
 
 (global-set-key (kbd "C-c j") 'open-junk-file)
