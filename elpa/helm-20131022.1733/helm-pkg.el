653dbb201132bc311a21237a171b4c1bf0d2728e
Naruse Motoki <motoki@naru.se>
2013-10-30 21:28:59 +0900

    パッケージのアップデート

--- elpa/helm-20131022.1733/helm-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "helm" "20131022.1733" "Helm is an Emacs incremental and narrowing framework [github]" 'nil)
-
--- elpa/web-mode-20131023.13/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "web-mode" "20131023.13" "major mode for editing html templates [github]" 'nil)
-
