502ee88a833c8f585cf0e97895448cdaf97c76f9
Naruse Motoki <motoki@naru.se>
2013-11-22 17:04:00 +0900

    PHPのインデントの設定を追加

---
+++ inits/40_php.el
@@ -0,0 +1,11 @@
+(add-hook 'php-mode-hook (lambda ()
+    (defun ywb-php-lineup-arglist-intro (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (+ (current-column) c-basic-offset))))
+    (defun ywb-php-lineup-arglist-close (langelem)
+      (save-excursion
+        (goto-char (cdr langelem))
+        (vector (current-column))))
+    (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+    (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
