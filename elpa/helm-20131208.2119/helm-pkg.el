7bb6003df38a3280a8aff18e4dd20c9e5c2dc6e0
Naruse Motoki <motoki@naru.se>
2013-12-16 12:44:08 +0900

    パッケージのアップデート

--- elpa/helm-20131208.2119/helm-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "helm" "20131208.2119" "Helm is an Emacs incremental and narrowing framework" 'nil)
--- elpa/twittering-mode-20131208.730/twittering-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "twittering-mode" "20131208.730" "Major mode for Twitter" (quote nil))
