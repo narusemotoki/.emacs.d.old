c5f8398c6185738da49fc18c9701d77c489faaac
Naruse Motoki <motoki@naru.se>
2013-12-31 14:00:56 +0900

    パッケージのアップデート

--- elpa/coffee-mode-20131226.1919/coffee-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "coffee-mode" "20131226.1919" "Major mode to edit CoffeeScript files in Emacs" (quote ((emacs "24.1"))))
--- elpa/web-mode-20131227.1242/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20131227.1242" "major mode for editing html templates" 'nil)
