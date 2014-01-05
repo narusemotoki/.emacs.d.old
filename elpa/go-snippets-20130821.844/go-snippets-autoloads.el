7b7c5f5b11758ba6ebb51bbb8b7149ed2e6aa12f
Naruse Motoki <motoki@naru.se>
2014-01-05 14:39:38 +0900

    Goの環境を追加

---
+++ elpa/go-snippets-20130821.844/go-snippets-autoloads.el
@@ -0,0 +1,31 @@
+;;; go-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (go-snippets-initialize) "go-snippets" "go-snippets.el"
+;;;;;;  (21192 61134 916447 959000))
+;;; Generated autoloads from go-snippets.el
+
+(autoload 'go-snippets-initialize "go-snippets" "\
+
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(go-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("go-snippets-pkg.el") (21192 61134 922911
+;;;;;;  287000))
+
+;;;***
+
+(provide 'go-snippets-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-snippets-autoloads.el ends here
