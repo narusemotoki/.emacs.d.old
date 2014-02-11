c696e97a15658186319885ca0d821d33d7103167
Naruse Motoki <motoki@naru.se>
2014-02-12 00:18:44 +0900

    パッケージのアップデート

--- elpa/web-mode-20140201.1600/web-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; web-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (web-mode) "web-mode" "web-mode.el" (21230 20702
-;;;;;;  447064 568000))
-;;; Generated autoloads from web-mode.el
-
-(autoload 'web-mode "web-mode" "\
-Major mode for editing web templates (HTML documents with embedded parts and blocks).
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("web-mode-pkg.el" "wfs-mode.el") (21230
-;;;;;;  20702 469413 489000))
-
-;;;***
-
-(provide 'web-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; web-mode-autoloads.el ends here
