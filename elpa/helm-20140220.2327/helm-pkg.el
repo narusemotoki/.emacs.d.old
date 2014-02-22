e13ae45e43de3f37156ab580e65437cf9817aa08
Naruse Motoki <motoki@naru.se>
2014-02-22 09:46:01 +0900

    パッケージのアップデート

---
+++ elpa/helm-20140220.2327/helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm" "20140220.2327" "Helm is an Emacs incremental and narrowing framework" 'nil)
--- elpa/init-loader-20130305.2231/init-loader-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "init-loader" "20130305.2231" "Loader for configuration files" (quote nil))
