f90a89bcabbb2cef6417e75b75674d2820aa8650
Naruse Motoki <motoki@naru.se>
2014-03-05 10:24:02 +0900

    パッケージのアップデート

--- elpa/concurrent-20130930.607/concurrent-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "concurrent" "20130930.607" "Concurrent utility functions for emacs lisp" (quote ((deferred "0.3.1"))))
---
+++ elpa/web-mode-20140304.1651/web-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "web-mode" "20140304.1651" "major mode for editing html templates" 'nil)
