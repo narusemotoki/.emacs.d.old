c9ad7578cc94b6138f50c61a9b4cb0774b1aa5a8
Naruse Motoki <motoki@naru.se>
2014-04-26 00:48:50 +0900

    パッケージのアップデート

--- elpa/twittering-mode-20140330.852/twittering-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; twittering-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (twit) "twittering-mode" "twittering-mode.el" (21306
-;;;;;;  24286 266021 453000))
-;;; Generated autoloads from twittering-mode.el
-
-(autoload 'twit "twittering-mode" "\
-Start twittering-mode.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("twittering-mode-pkg.el") (21306 24286
-;;;;;;  322405 425000))
-
-;;;***
-
-(provide 'twittering-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; twittering-mode-autoloads.el ends here
