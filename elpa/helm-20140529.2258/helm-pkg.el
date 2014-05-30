d74f6521cfebcf9e53e63c8d6b660ed9187522a9
Naruse Motoki <motoki@naru.se>
2014-05-31 08:49:17 +0900

    パッケージのアップデート

---
+++ elpa/helm-20140529.2258/helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm" "20140529.2258" "Helm is an Emacs incremental and narrowing framework" 'nil)
---
+++ elpa/json-snatcher-20140527.2147/json-snatcher-autoloads.el
@@ -0,0 +1,29 @@
+;;; json-snatcher-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (jsons-print-path) "json-snatcher" "json-snatcher.el"
+;;;;;;  (21385 6349 415615 683000))
+;;; Generated autoloads from json-snatcher.el
+
+(autoload 'jsons-print-path "json-snatcher" "\
+Print the path to the JSON value under point, and save it in the kill ring.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("json-snatcher-pkg.el") (21385 6349 443161
+;;;;;;  657000))
+
+;;;***
+
+(provide 'json-snatcher-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; json-snatcher-autoloads.el ends here
