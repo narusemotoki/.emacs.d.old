f446d0043d42ee4240ad58c33753e14204ed722b
Naruse Motoki <motoki@naru.se>
2014-06-21 03:39:08 +0900

    esupを追加

---
+++ elpa/esup-20130607.1350/esup-autoloads.el
@@ -0,0 +1,27 @@
+;;; esup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (esup) "esup" "esup.el" (21412 32401 3649 676000))
+;;; Generated autoloads from esup.el
+
+(autoload 'esup "esup" "\
+Profile the startup time of Emacs in the background.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("esup-pkg.el") (21412 32401 51404 788000))
+
+;;;***
+
+(provide 'esup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; esup-autoloads.el ends here
