33275a13eca53ff8449280eae552e06b640d6c5b
Naruse Motoki <motoki@naru.se>
2014-06-22 15:23:20 +0900

    パッケージのアップデート

--- elpa/fringe-helper-20130519.1641/fringe-helper-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; fringe-helper-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("fringe-helper-pkg.el" "fringe-helper.el"
-;;;;;;  "tests.el") (21054 35438 765619))
-
-;;;***
-
-(provide 'fringe-helper-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; fringe-helper-autoloads.el ends here
