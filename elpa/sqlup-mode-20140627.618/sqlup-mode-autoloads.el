c61be9804cd9dfe9b6d6350612fca945d849bad8
Naruse Motoki <motoki@naru.se>
2014-06-28 14:19:49 +0900

    sqlup-modeを追加

---
+++ elpa/sqlup-mode-20140627.618/sqlup-mode-autoloads.el
@@ -0,0 +1,18 @@
+;;; sqlup-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("sqlup-mode-pkg.el" "sqlup-mode.el") (21422
+;;;;;;  20563 635947 830000))
+
+;;;***
+
+(provide 'sqlup-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sqlup-mode-autoloads.el ends here
---
+++ elpa/sqlup-mode-20140627.618/sqlup-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "sqlup-mode" "20140627.618" "Upcase SQL words for you" (quote nil))
