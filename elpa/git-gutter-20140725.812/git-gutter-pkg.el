5c67b3453d3a4e3a005520e7c3637f1b680f759f
Naruse Motoki <motoki@naru.se>
2014-07-26 22:02:39 +0900

    パッケージのアップデート

---
+++ elpa/git-gutter-20140725.812/git-gutter-pkg.el
@@ -0,0 +1,1 @@
+(define-package "git-gutter" "20140725.812" "Port of Sublime Text plugin GitGutter" (quote ((cl-lib "0.5") (emacs "24"))))
---
+++ elpa/helm-20140725.2249/helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm" "20140725.2249" "Helm is an Emacs incremental and narrowing framework" 'nil)
