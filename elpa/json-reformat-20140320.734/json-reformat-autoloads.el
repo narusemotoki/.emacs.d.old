4c53f4ff026f72049635e232bd247fdb503d7023
Naruse Motoki <motoki@naru.se>
2014-10-11 11:56:06 +0900

    パッケージのアップデート

--- elpa/json-reformat-20140320.734/json-reformat-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; json-reformat-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (json-reformat-region) "json-reformat" "json-reformat.el"
-;;;;;;  (21300 1534 528689 956000))
-;;; Generated autoloads from json-reformat.el
-
-(autoload 'json-reformat-region "json-reformat" "\
-Reformat the JSON in the region from BEGIN to END.
-
-\(fn BEGIN END)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("json-reformat-pkg.el") (21300 1534 558597
-;;;;;;  179000))
-
-;;;***
-
-(provide 'json-reformat-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; json-reformat-autoloads.el ends here
--- elpa/web-mode-20141007.1506/web-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; web-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (web-mode) "web-mode" "web-mode.el" (21556 45002
-;;;;;;  254153 829000))
-;;; Generated autoloads from web-mode.el
-
-(autoload 'web-mode "web-mode" "\
-Major mode for editing web templates.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("web-mode-pkg.el" "wfs-mode.el") (21556
-;;;;;;  45002 310747 130000))
-
-;;;***
-
-(provide 'web-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; web-mode-autoloads.el ends here
