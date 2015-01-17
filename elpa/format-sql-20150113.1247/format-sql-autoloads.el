ca18ac908f13fea77e059ca59bb38d7bae3272ea
Naruse Motoki <motoki@naru.se>
2015-01-17 22:53:12 +0900

    format-sqlを追加

---
+++ elpa/format-sql-20150113.1247/format-sql-autoloads.el
@@ -0,0 +1,39 @@
+;;; format-sql-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (format-sql-buffer format-sql-region format-sql)
+;;;;;;  "format-sql" "format-sql.el" (21690 26942 178629 849000))
+;;; Generated autoloads from format-sql.el
+
+(autoload 'format-sql "format-sql" "\
+Uses the \"format-sql\" tool to reformat the current buffer.
+
+\(fn &optional ONLY-ON-REGION)" t nil)
+
+(autoload 'format-sql-region "format-sql" "\
+Uses the \"format-sql\" tool to reformat the current region.
+
+\(fn)" t nil)
+
+(autoload 'format-sql-buffer "format-sql" "\
+Uses the \"format-sql\" tool to reformat the current buffer.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("format-sql-pkg.el") (21690 26942 211554
+;;;;;;  579000))
+
+;;;***
+
+(provide 'format-sql-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; format-sql-autoloads.el ends here
---
+++ elpa/format-sql-20150113.1247/format-sql-pkg.el
@@ -0,0 +1,1 @@
+(define-package "format-sql" "20150113.1247" "Use format-sql to make your SQL readable in directly Emacs." (quote nil))
