2b1c5bbe60cd23dce700c70253bc7d8048056b48
Naruse Motoki <motoki@naru.se>
2015-03-31 19:04:47 +0900

    Auto saveを30秒待ちにした

--- inits/10_auto-save-buffers-enhanced.el
+++ inits/10_auto-save-buffers-enhanced.el
@@ -3,5 +3,5 @@
 ;------------------------------------------------------------------------------
 (require 'auto-save-buffers-enhanced)
 ; ここで指定した秒数の間入力がないと自動保存する
-(setq auto-save-buffers-enhanced-interval 0.5)
+(setq auto-save-buffers-enhanced-interval 30)
 (auto-save-buffers-enhanced t)
